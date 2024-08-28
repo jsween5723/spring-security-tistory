1e3796cb3b4d02c0b8202ade0e98ee0d71460168
jsween5723 <jsween5723@gmail.com>
2024-08-29 01:29:14 +0900

    init

---
+++ src/main/java/example/springsecuritytistory/SpringsecuritytistoryApplication.java
@@ -0,0 +1,13 @@
+package example.springsecuritytistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritytistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecuritytistoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/springsecuritytistory/SpringsecuritytistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package example.springsecuritytistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritytistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
