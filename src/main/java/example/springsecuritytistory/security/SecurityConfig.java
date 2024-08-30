a679788df00acfb04b58943f0c6200550cf39c9c
jsween5723 <gusckd57@naver.com>
2024-08-31 00:33:55 +0900

    2. Spring Security 일반 로그인

--- src/main/java/example/springsecuritytistory/security/SecurityConfig.java
+++ src/main/java/example/springsecuritytistory/security/SecurityConfig.java
@@ -1,14 +1,23 @@
 package example.springsecuritytistory.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import example.springsecuritytistory.common.ExceptionResponse;
+import example.springsecuritytistory.common.Response;
 import example.springsecuritytistory.repository.UserEntity;
 import example.springsecuritytistory.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +25,38 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 class SecurityConfig {
 
     private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable).formLogin(
+                conf -> conf.loginProcessingUrl("/api/v1/auth/login").successHandler(
+                    ((request, response, authentication) -> sendResponse(response, "로그인 성공")))
+                    .failureHandler(this::entryPoint))
+            .exceptionHandling(e -> e.authenticationEntryPoint(this::entryPoint)
+                .accessDeniedHandler(this::accessDeniedHandler));
+        return http.build();
+    }
+
+    private void entryPoint(HttpServletRequest request, HttpServletResponse response,
+        RuntimeException e) throws IOException {
+        response.setStatus(401);
+        sendResponse(response, ExceptionResponse.of(e));
+    }
+
+    private void accessDeniedHandler(HttpServletRequest request, HttpServletResponse response,
+        RuntimeException e) throws IOException {
+        response.setStatus(403);
+        sendResponse(response, ExceptionResponse.of(e));
+    }
+
+    private <T> void sendResponse(HttpServletResponse response, T e) throws IOException {
+        Response<T> body = new Response<>(e);
+        String bodyString = objectMapper.writeValueAsString(body);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(bodyString);
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
