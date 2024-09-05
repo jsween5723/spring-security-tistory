438b530a382c9fc1071e3769bb68f14f051a2dad
jsween5723 <gusckd57@naver.com>
2024-09-05 19:08:03 +0900

    5. 로그인 시 토큰 반환

--- src/main/java/example/springsecuritytistory/service/Provider.java
+++ src/main/java/example/springsecuritytistory/service/Provider.java
@@ -1,8 +1,17 @@
 package example.springsecuritytistory.service;
 
 import example.springsecuritytistory.security.UserRole;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import org.apache.logging.log4j.util.Strings;
 
 public record Provider(long userId, String username, Set<UserRole> roles) {
-
+    public Map<String, Object> toClaims() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", username);
+        claims.put("sub", String.valueOf(userId));
+        claims.put("authorities", Strings.join(roles, ' '));
+        return claims;
+    }
 }
